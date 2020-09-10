4536d220853173536c06d5acf0db3875875abe14
Administrator <admin@example.com>
2020-09-10 19:32:47 +0800

    feat: 新增JDK7之前的文件拷贝功能Demo

---
+++ jamie-jdk8/lib/FileCopyTest.java
@@ -0,0 +1,58 @@
+package com.jamie.jdk8.resource;
+
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * JDK7之前的文件拷贝功能
+ * @author jamie
+ * @date 2020/9/10 19:11
+ */
+public class FileCopyTest {
+
+    @Test
+    public void copyFile() {
+        // 1.创建输入输出流
+        // 2.执行文件拷贝，读取文件内容，写入到另一个文件中
+        // 3.关闭文件流资源
+
+        // 定义输入路径和输出路径
+        String originalUrl = "lib\\FileCopyTesr.java";
+        String targetUrl = "targetTest\\FileCopyTesr.java";
+        // 声明文件输入流，文件输出流
+        FileInputStream originalFileInputStream = null;
+        FileOutputStream targetFileOutputStream = null;
+        try{
+            // 实例化文件流对象
+            originalFileInputStream = new FileInputStream(originalUrl);
+            targetFileOutputStream = new FileOutputStream(targetUrl);
+            // 读取的字节信息
+            int content;
+            // 迭代，读取/写入字节
+            while((content = originalFileInputStream.read()) != -1){
+                targetFileOutputStream.write(content);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally{
+            if(targetFileOutputStream != null){
+                try{
+                    targetFileOutputStream.close();
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+            if(originalFileInputStream != null){
+                try{
+                    originalFileInputStream.close();
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
